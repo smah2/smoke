184239a9413fcae3a3bdf4fd41d5cbae48465cef
smah2 <mr.hossini@mihanmail.ir>
2016-05-31 21:33:29 +0430

    translate to persian
    Hi
    translated to persian to put in your great pakage
    tnx

---
+++ dist/lang/fa.js
@@ -0,0 +1,31 @@
+
+// Translated By https://github.com/smah2 
+
+(function($){
+  $.fn.smkValidate.Languaje = {
+    textEmpty        : 'فیلدهای ضروری ',
+    textEmail        : 'یک ایمیل معتبر را وارد نمایید ',
+    textAlphanumeric : 'تنها اعداد و/یا حروف مجاز می‌باشد ',
+    textNumber       : 'تنها اعداد مجاز می‌باشد',
+    textNumberRange  : 'محدوده عددی باید بیشتر از  <b> {@} </b> و کمتر از <b> {@} </b> باشد ',
+    textDecimal      : 'تنها اعداد اعشاری مجاز می‌باشد',
+    textCurrency     : 'لطفا مقدار پولی معتبر وارد نمایید ',
+    textSelect       : 'حداقل یک گزینه انتخاب فرمایید.',
+    textCheckbox     : 'حداقل یک گزینه انتخاب فرمایید.',
+    textLength       : 'تعداد کاراکتر = <b> {@} </b>',
+    textRange        : 'تعداد کاراکترها بایست بیشتر از <b> {@} </b> و کمتر از <b> {@} </b> باشد',
+    textSPassDefault : 'حداقل 4 کاراکتر',
+    textSPassWeak    : 'حداقل 6 کاراکتر',
+    textSPassMedium  : 'حداقل 6 کاراکتر و یک عدد'
+  };
+
+  $.smkEqualPass.Languaje = {
+    textEqualPass    : 'رمز صحیح را وارد نمایید'
+  };
+
+  $.smkDate.Languaje = {
+    shortMonthNames : ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+    monthNames : ["ژانویه", "فبریه", "مارس", "آوریل", "می", "جوئن", "جولای", "آگوست", "سپتامبر", "اکتبر", "نوامبر", "دسامبر"]
+  };
+
+}(jQuery));
